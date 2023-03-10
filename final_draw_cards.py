c899bbb217c8719f6fd0090750905cadfc5166f1
is13 <is13mozg@gmail.com>
2023-03-10 19:21:51 +0300

    make true size 2

--- final_draw_cards.py
+++ final_draw_cards.py
@@ -2,22 +2,24 @@ from PIL import Image
 from PIL import ImageDraw, ImageFont
 import cards_list
 
-ih= 1297
-iw = 1925 #1836
+ih = 1297
+iw = 1925  # 1836
+
+
 def draw_9(name):
     mim = Image.new('RGBA', (ih, iw), "white")
-    pim= Image.open(name)
-    mim.paste(pim,(-2,20),pim)
-    mim.paste(pim, (-2, 575),pim)
-    mim.paste(pim, (-2, 1115),pim)
+    pim = Image.open(name)
+    mim.paste(pim, (-2, 20), pim)
+    mim.paste(pim, (-2, 575), pim)
+    mim.paste(pim, (-2, 1115), pim)
 
-    mim.paste(pim,(815,20),pim)
-    mim.paste(pim, (815, 575),pim)
-    mim.paste(pim, (815, 1115),pim)
+    mim.paste(pim, (815, 20), pim)
+    mim.paste(pim, (815, 575), pim)
+    mim.paste(pim, (815, 1115), pim)
 
-    mim.paste(pim, (408, 277),pim)
-    mim.paste(pim, (408, 844),pim)
-    mim.paste(pim, (408, 1350),pim)
+    mim.paste(pim, (408, 277), pim)
+    mim.paste(pim, (408, 844), pim)
+    mim.paste(pim, (408, 1350), pim)
     return mim
 
 
@@ -51,6 +53,7 @@ def draw_7(name):
     mim.paste(pim, (770, 345), pim)
     mim.paste(pim, (770, 995), pim)
     return mim
+
 
 def draw_6(name):
     mim = Image.new('RGBA', (ih, iw), "white")
@@ -93,30 +96,32 @@ def draw_4(name):
 
     return mim
 
+
 def draw_3(name):
     mim = Image.new('RGBA', (ih, iw), "white")
-    pim= Image.open(name)
+    pim = Image.open(name)
 
-    mim.paste(pim, (400, 110),pim)
-    mim.paste(pim, (400, 635),pim)
-    mim.paste(pim, (400, 1175),pim)
+    mim.paste(pim, (400, 110), pim)
+    mim.paste(pim, (400, 635), pim)
+    mim.paste(pim, (400, 1175), pim)
     return mim
 
 
 def draw_2(name):
     mim = Image.new('RGBA', (ih, iw), "white")
-    pim= Image.open(name)
+    pim = Image.open(name)
 
-    mim.paste(pim, (400, 310),pim)
-    mim.paste(pim, (400, 1075),pim)
+    mim.paste(pim, (400, 310), pim)
+    mim.paste(pim, (400, 1075), pim)
     return mim
+
 
 def draw_2(name):
     mim = Image.new('RGBA', (ih, iw), "white")
-    pim= Image.open(name)
+    pim = Image.open(name)
 
-    mim.paste(pim, (400, 310),pim)
-    mim.paste(pim, (400, 1075),pim)
+    mim.paste(pim, (400, 310), pim)
+    mim.paste(pim, (400, 1075), pim)
     return mim
 
 
@@ -127,20 +132,19 @@ def draw_1(name):
     return mim
 
 
-def draw_all(a,b,c,d,count):
+def draw_all(a, b, c, d, count):
     fname = f"figures\{a}_1_{c}_{d}.png"
-    dict ={0:draw_1,
-           1:draw_2,
-           2:draw_3,
-           3:draw_4,
-           4:draw_5,
-           5:draw_6,
-           6:draw_7,
-           7:draw_8,
-           8:draw_9}
-    f=dict[b];
-    res=f(fname)
-
+    dict = {0: draw_1,
+            1: draw_2,
+            2: draw_3,
+            3: draw_4,
+            4: draw_5,
+            5: draw_6,
+            6: draw_7,
+            7: draw_8,
+            8: draw_9}
+    f = dict[b];
+    res = f(fname)
 
     draw = ImageDraw.Draw(res)
     font = ImageFont.truetype('Xolonium-Bold.otf', size=66)
@@ -151,15 +155,12 @@ def draw_all(a,b,c,d,count):
     draw = ImageDraw.Draw(res)
     draw.text((40, 40), "№_" + str(count), font=font, fill='#000')
     draw.text((1000, 40), "№_" + str(count), font=font, fill='#000')
+    res = res.resize((1186, 1762))
 
     res.save(f"res\{a}_{b}_{c}_{d}.png", quality=100, subsampling=0)
 
 
-
-
-
-
-#(a,b,c,d)
+# (a,b,c,d)
 # какая фигура 0 уголник, 1 угольник и тд макс 7 уугольник 0..7 mod 8
 # кол-во фигур на карточке от 1 до 9  0..8  mod 9
 # цвет фигур от красного до фиолетового КОЖЗГСФ  0..6 mod 7
@@ -173,7 +174,10 @@ def draw_all(a,b,c,d,count):
 # d = (d + 1) % 6
 def mf():
     main_ls = cards_list.main_ls
-    count=0
+    count = 0
     for x in main_ls:
-        draw_all(x[0],x[1],x[2],x[3],count+1)
-        count+=1
+        draw_all(x[0], x[1], x[2], x[3], count + 1)
+        count += 1
+
+
+mf()
--- gen_img_for_print.py
+++ gen_img_for_print.py
@@ -3,7 +3,7 @@ from PIL import ImageDraw, ImageFont
 
 import cards_list
 
-
+main_ls = cards_list.main_ls
 
 
 # (a,b,c,d)
@@ -25,9 +25,10 @@ def num_by_card(card):
     return main_ls.index(card) + 1
 
 
-
-
 def mf():
+    iw = 1186  # 1297
+    ih = 1762  # 1925  # ih
+    line = 18
 
     main_ls = cards_list.main_ls
 
@@ -40,40 +41,40 @@ def mf():
         t_ls = main_ls[i * 9:(i + 1) * 9]
         print(*t_ls)
         print(i * 9, (i + 1) * 9)
-        mim = Image.new('RGBA', (1297 * 3, 1836 * 3), "white")
+        mim = Image.new('RGBA', (1297 * 3, 1925 * 3), "white")
 
         pim = Image.open(f"res\{t_ls[0][0]}_{t_ls[0][1]}_{t_ls[0][2]}_{t_ls[0][3]}.png")
-        mim.paste(pim, (1297 * 0, 1836 * 0), pim)
+        mim.paste(pim, ((iw + line) * 0, (ih + line) * 0), pim)
 
         pim = Image.open(f"res\{t_ls[1][0]}_{t_ls[1][1]}_{t_ls[1][2]}_{t_ls[1][3]}.png")
-        mim.paste(pim, (1297 * 1, 1836 * 0), pim)
+        mim.paste(pim, ((iw + line) * 1, (ih + line) * 0), pim)
 
         pim = Image.open(f"res\{t_ls[2][0]}_{t_ls[2][1]}_{t_ls[2][2]}_{t_ls[2][3]}.png")
-        mim.paste(pim, (1297 * 2, 1836 * 0), pim)
+        mim.paste(pim, ((iw + line) * 2, (ih + line) * 0), pim)
 
         pim = Image.open(f"res\{t_ls[3][0]}_{t_ls[3][1]}_{t_ls[3][2]}_{t_ls[3][3]}.png")
-        mim.paste(pim, (1297 * 0, 1836 * 1), pim)
+        mim.paste(pim, ((iw + line) * 0, (ih + line) * 1), pim)
 
         pim = Image.open(f"res\{t_ls[4][0]}_{t_ls[4][1]}_{t_ls[4][2]}_{t_ls[4][3]}.png")
-        mim.paste(pim, (1297 * 1, 1836 * 1), pim)
+        mim.paste(pim, ((iw + line) * 1, (ih + line) * 1), pim)
 
         pim = Image.open(f"res\{t_ls[5][0]}_{t_ls[5][1]}_{t_ls[5][2]}_{t_ls[5][3]}.png")
-        mim.paste(pim, (1297 * 2, 1836 * 1), pim)
+        mim.paste(pim, ((iw + line) * 2, (ih + line) * 1), pim)
 
         pim = Image.open(f"res\{t_ls[6][0]}_{t_ls[6][1]}_{t_ls[6][2]}_{t_ls[6][3]}.png")
-        mim.paste(pim, (1297 * 0, 1836 * 2), pim)
+        mim.paste(pim, ((iw + line) * 0, (ih + line) * 2), pim)
 
         pim = Image.open(f"res\{t_ls[7][0]}_{t_ls[7][1]}_{t_ls[7][2]}_{t_ls[7][3]}.png")
-        mim.paste(pim, (1297 * 1, 1836 * 2), pim)
+        mim.paste(pim, ((iw + line) * 1, (ih + line) * 2), pim)
 
         pim = Image.open(f"res\{t_ls[8][0]}_{t_ls[8][1]}_{t_ls[8][2]}_{t_ls[8][3]}.png")
-        mim.paste(pim, (1297 * 2, 1836 * 2), pim)
+        mim.paste(pim, ((iw + line) * 2, (ih + line) * 2), pim)
 
         draw = ImageDraw.Draw(mim)
-        draw.line((1297 * 1, 1836 * 0, 1297 * 1, 1836 * 3), fill="black", width=8)
-        draw.line((1297 * 2, 1836 * 0, 1297 * 2, 1836 * 3), fill="black", width=8)
-        draw.line((1297 * 0, 1836 * 1, 1297 * 3, 1836 * 1), fill="black", width=8)
-        draw.line((1297 * 0, 1836 * 2, 1297 * 3, 1836 * 2), fill="black", width=8)
+        draw.line((iw * 1, ih * 0, iw * 1, ih * 3), fill="black", width=8)
+        draw.line((iw * 2 + line, ih * 0, iw * 2 + line, ih * 3), fill="black", width=8)
+        draw.line((iw * 0, ih * 1, iw * 3, ih * 1), fill="black", width=8)
+        draw.line((iw * 0, ih * 2 + line, iw * 3, ih * 2 + line), fill="black", width=8)
 
         bg = Image.new("RGB", mim.size, (255, 255, 255))
         bg.paste(mim, mask=mim.split()[3])
@@ -84,3 +85,5 @@ def mf():
     f_im = Image.open("res2/0.jpg")
     to_pdf_ls = to_pdf_ls[1:]
     f_im.save("res2\mainpdf.pdf", "PDF", resolution=100.0, save_all=True, append_images=to_pdf_ls)
+
+mf()
